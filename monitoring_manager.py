23acd043e01e01c31403890fe1ec2cf5e122c777
Nikhil Manchanda <SlickNik@gmail.com>
2014-11-20 19:18:08 -0800

    Refactored the monitoring-manager

--- monitoring_manager.py
+++ monitoring_manager.py
@@ -1,4 +1,4 @@
-# Copyright 2011 Hewlett Packard
+# Copyright 2014 Hewlett Packard
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -14,6 +14,7 @@
 #    under the License.
 
 import trove.common.cfg as cfg
+from trove.common import instance
 from trove.instance import models as t_models
 from trove.instance.tasks import InstanceTasks
 from trove.instance.models import InstanceServiceStatus
@@ -31,6 +32,80 @@ FAILOVER_ACTIVE_TIME_DELTA = datetime.timedelta(minutes=15)
 
 class MonitoringManager(manager.Manager):
 
+    def _engage_failover(self, service):
+        now = datetime.datetime.now()
+        return ((service.status == instance.ServiceStatuses.RUNNING and
+                 now > service.updated_at + FAILOVER_ACTIVE_TIME_DELTA) or
+                (service.status == instance.ServiceStatuses.SHUTDOWN and
+                 now > service.updated_at + FAILOVER_FAILED_TIME_DELTA))
+
+    def _create_and_auth_clients(self):
+        from troveclient.v1.client import Client as tclient
+        from designateclient.v1 import Client as dclient
+        from novaclient.v1_1.client import Client as nclient
+
+        self.trove_client = tclient(CONF.nova_proxy_admin_user,
+                                    CONF.nova_proxy_admin_pass,
+                                    CONF.nova_proxy_admin_tenant_name,
+                                    auth_url=CONF.trove_auth_url,
+                                    service_type='database',
+                                    region_name=CONF.os_region_name)
+        self.trove_client.authenticate()
+
+        self.nova_client = nclient(CONF.nova_proxy_admin_user,
+                                   CONF.nova_proxy_admin_pass,
+                                   CONF.nova_proxy_admin_tenant_name,
+                                   auth_url=CONF.trove_auth_url,
+                                   service_type='compute',
+                                   region_name=CONF.os_region_name)
+        self.nova_client.authenticate()
+
+        self.designate_client = dclient(username=CONF.dns_username,
+                                        password=CONF.dns_passkey,
+                                        auth_url=CONF.dns_auth_url,
+                                        tenant_id=CONF.dns_account_id,
+                                        region_name=CONF.dns_region)
+
+    def _update_dns_records(self, master, slave):
+        # Get compute node information
+        master_compute = self.nova_client.servers.get(
+            master.compute_instance_id)
+        slave_compute = self.nova_client.servers.get(
+            slave.compute_instance_id)
+
+        # Find master DNS record
+        master_dns_record = [
+            record_id for record_id in
+            self.designate_client.records.list(CONF.dns_domain_id)
+            if (record_id.type == 'A' and
+                master_compute.name in record_id.name)].pop()
+
+        # Find slave DNS record
+        slave_dns_record = [
+            record_id for record_id in
+            self.designate_client.records.list(CONF.dns_domain_id)
+            if (record_id.type == 'A' and
+                slave_compute.name in record_id.name)].pop()
+
+        # Update DNS records for master and slave
+        # We choose to swap them here
+        master_dns_record.data, slave_dns_record.data = \
+            slave_dns_record.data, master_dns_record.data
+
+        self.designate_client.records.update(CONF.dns_domain_id,
+                                             slave_dns_record)
+        self.designate_client.records.update(CONF.dns_domain_id,
+                                             master_dns_record)
+
+        return master_dns_record, slave_dns_record
+
+    def _reflect_dns_updates_in_trove(self, master, master_dns,
+                                      slave, slave_dns):
+        setattr(master, 'hostname', slave_dns.name)
+        setattr(slave, 'hostname', master_dns.name)
+        master.save()
+        slave.save()
+
     @periodic_task.periodic_task(ticks_between_runs=2)
     def monitor_ha(self, context):
         """Monitors the status of MySQL masters to make sure they are up."""
@@ -45,77 +120,23 @@ class MonitoringManager(manager.Manager):
         for (master_id, slave) in masters_to_watch:
             master = t_models.DBInstance.find_by(deleted=False, id=master_id)
             service = InstanceServiceStatus.find_by(instance_id=master_id)
-            now = datetime.datetime.now()
-            if ((service.status_id == 1L and now > service.updated_at +
-                FAILOVER_ACTIVE_TIME_DELTA)
-                or
-                (service.status_id == 4L and now > service.updated_at +
-                 FAILOVER_FAILED_TIME_DELTA)):
+
+            if self._engage_failover(service):
                 LOG.debug("Engage FAILOVER from %s to %s NOW!",
                           master_id, slave.id)
                 master = t_models.DBInstance.find_by(id=master_id,
                                                      deleted=False)
 
-                from troveclient.v1.client import Client as tclient
-                from designateclient.v1 import Client as dclient
-                from novaclient.v1_1.client import Client as nclient
-
-                trove_client = tclient(CONF.nova_proxy_admin_user,
-                                       CONF.nova_proxy_admin_pass,
-                                       CONF.nova_proxy_admin_tenant_name,
-                                       auth_url=CONF.trove_auth_url,
-                                       service_type='database',
-                                       region_name=CONF.os_region_name)
-                trove_client.authenticate()
-
-                nova_client = nclient(CONF.nova_proxy_admin_user,
-                                      CONF.nova_proxy_admin_pass,
-                                      CONF.nova_proxy_admin_tenant_name,
-                                      auth_url=CONF.trove_auth_url,
-                                      service_type='compute',
-                                      region_name=CONF.os_region_name)
-                nova_client.authenticate()
-                master_compute = nova_client.servers.get(
-                    master.compute_instance_id)
-                slave_compute = nova_client.servers.get(
-                    slave.compute_instance_id)
-
-                designate_client = dclient(username=CONF.dns_username,
-                                           password=CONF.dns_passkey,
-                                           auth_url=CONF.dns_auth_url,
-                                           tenant_id=CONF.dns_account_id,
-                                           region_name=CONF.dns_region)
+                self._create_and_auth_clients()
 
                 # Failover Slave to Master by detaching replica source on slave
-                trove_client.instances.edit(slave.id,
-                                            detach_replica_source=True)
+                self.trove_client.instances.edit(slave.id,
+                                                 detach_replica_source=True)
 
-                # Find master DNS record
-                master_dns_record = [
-                    record_id for record_id in
-                    designate_client.records.list(CONF.dns_domain_id)
-                    if (record_id.type == 'A' and
-                        master_compute.name in record_id.name)].pop()
-
-                # Find slave DNS record
-                slave_dns_record = [
-                    record_id for record_id in
-                    designate_client.records.list(CONF.dns_domain_id)
-                    if (record_id.type == 'A' and
-                        slave_compute.name in record_id.name)].pop()
-
-                # Swap DNS records for master and slave
-                master_dns_record.data, slave_dns_record.data = \
-                    slave_dns_record.data, master_dns_record.data
-
-                designate_client.records.update(CONF.dns_domain_id,
-                                                slave_dns_record)
-                designate_client.records.update(CONF.dns_domain_id,
-                                                master_dns_record)
+                # Update DNS records for master and slave
+                master_dns, slave_dns = self._update_dns_records(master, slave)
 
                 # Finally update the hostnames in trove to
                 # reflect the updated DNS information
-                setattr(master, 'hostname', slave_compute.name)
-                setattr(slave, 'hostname', master_compute.name)
-                master.save()
-                slave.save()
+                self._reflect_dns_updates_in_trove(master, master_dns,
+                                                   slave, slave_dns)
